20b1a91f88e53561fce652ecd65ca7f80a91f37a
Sierra Heins <sierra.heins@evergy.com>
2022-05-23 14:31:19 -0500

    Module 6

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -7,6 +7,7 @@ from temperature_info import TemperatureData
 from humidity_info import HumidityData
 from statistics import mean
 from particle_count_info import ParticleData
+from energy_info import EnergyData
 ##############################
 # Do not remove these two lines
 # They are needed to validate your unittest
@@ -60,11 +61,9 @@ print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
 
 # Module 6 code here:
-recs = particle_data.get_data_by_date(rec_date=test_date)
-print("\nHouse Particle sensor records for date: {} = {}".format(
+recs = energy_data.get_data_by_date(rec_date=test_date)
+print("House Energy sensor records for date: {} = {}".format(
     test_date.strftime("%m/%d/%y"), len(recs)))
-concentrations = particle_data.get_data_concentrations(data = recs)
-print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
-print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
-print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+total_energy = energy_data.calculate_energy_usage(data=recs)
+print("\tEnergy Usage: {:2.2} Watts".format(total_energy)) 
 # Module 5 code here: # Runner script for all modules
