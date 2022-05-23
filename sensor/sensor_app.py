1399f0c0d1e4f19fcc0c5176574c1f0e962679ef
Sierra Heins <sierra.heins@evergy.com>
2022-05-23 14:25:24 -0500

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,5 +1,6 @@
 # Runner script for all modules
 
+
 from load_data import load_sensor_data
 from house_info import HouseInfo
 from datetime import datetime, date
@@ -51,13 +52,7 @@ print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
 print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
 print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
 # Module 6 code here:
-recs = particle_data.get_data_by_date(rec_date=test_date)
-print("\nHouse Particle sensor records for date: {} = {}".format(
-    test_date.strftime("%m/%d/%y"), len(recs)))
-concentrations = particle_data.get_data_concentrations(data = recs)
-print("\tGood Air Quality Recs: {}".format(concentrations["good"]))
-print("\tModerate Air Quality Recs: {}".format(concentrations["moderate"]))
-print("\tBad Air Quality Recs: {}".format(concentrations["bad"]))
+
 # Module 5 code here: # Runner script for all modules
 
 
